e96474b9b0a57da9b6323a2b603292befdec3afc
Rui Rua <ruirua@Air-de-Rui.lan>
2021-06-22 18:04:45 +0100

    updated deploy and res

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
 rm -rf dist/*
-python -m incremental.update anadroid --patch
+python3 -m incremental.update anadroid --patch
 git add anadroid/_version.py
 git commit -m "bump _version"
-python setup.py sdist
-python -m twine upload dist/*
-git push origin dev
+python3 setup.py sdist
+python3 -m twine upload dist/*
+git push origin main
