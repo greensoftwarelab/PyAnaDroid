f899331176a5fdce8b07b273fd14eef90798bbac
Rui Rua <rui.rrua@gmail.com>
2022-04-06 15:41:01 +0100

    updated aux script

--- aux_scripts/language_counter.py
+++ aux_scripts/language_counter.py
@@ -1,11 +1,31 @@
 import json
 import os
+from os import listdir
+from subprocess import Popen, PIPE, TimeoutExpired
+
 import matplotlib.pyplot as plt
 from pylab import *
 
 from textops import find
 
 EXCLUDED_LANGS = {'gitignore', 'Markdown', 'License', 'JSON', 'YAML', 'Prolog', 'Batch', 'Properties File'}
+
+
+def execute_shell_command(cmd, args=[], timeout=None):
+    command = cmd + " " + " ".join(args) if len(args) > 0 else cmd
+    out = bytes()
+    err = bytes()
+    #print(command)
+    proc = Popen(command, stdout=PIPE, stderr=PIPE,shell=True)
+    try:
+        out, err = proc.communicate(timeout=timeout)
+    except TimeoutExpired as e:
+        print("command " + cmd + " timed out")
+        out = e.stdout if e.stdout is not None else out
+        err = e.stderr if e.stderr is not None else err
+        proc.kill()
+        proc.returncode = 1
+    return proc.returncode, out.decode("utf-8"), err.decode('utf-8')
 
 
 def mega_find(basedir, pattern="*", maxdepth=999, mindepth=0, type_file='n'):
@@ -21,7 +41,7 @@ class LanguageStats(object):
         self.parsed_files = set()
 
     def add_language_info(self, lang_info):
-        print(lang_info)
+        #print(lang_info)
         if 'Name' not in lang_info or lang_info['Name'] in EXCLUDED_LANGS:
             return
         if lang_info['Name'] in self.language_info:
@@ -83,7 +103,6 @@ class LanguageStats(object):
     def gen_langs_boxplots_cc(self):
         fig1, en_box = plt.subplots()
         the_list = [ list(map(lambda z: z['Complexity'], x)) for x in map(lambda t: t['proj_files'], self.language_info.values())]
-        print(the_list)
         bp_dict = en_box.boxplot(the_list, self.language_info.keys(), patch_artist=True)
         i = 0
         for line in bp_dict['medians']:
@@ -104,13 +123,66 @@ class LanguageStats(object):
         plt.show()
 
 
+def get_project_root_dir(proj_path):
+    """infers Android project root directory."""
+    has_gradle_right_next = mega_find(proj_path, pattern="build.gradle", maxdepth=4, type_file='f')
+    if len(has_gradle_right_next) > 0:
+        top_gradle_file = min(has_gradle_right_next, key=len)
+        return os.path.dirname(top_gradle_file)
+    return None
+
+
+def is_android_project(dirpath):
+    """determines if a given directory is an Android Project.
+    looks for settings.gradle files.
+    Args:
+        dirpath: path of the directory.
+
+    Returns:
+        bool: True if file is in diretory, False otherwise.
+    """
+    return "settings.gradle" in [f for f in listdir(dirpath)]
+
+
+
+def load_projects(dirpath):
+    """loads Android Projects from a directory containing one or more projects."""
+    return_projs = []
+    if is_android_project(dirpath):
+        potential_projects = [dirpath]
+    else:
+        potential_projects = list(
+            filter(lambda x: os.path.isdir(os.path.join(dirpath, x)), os.listdir(dirpath)))
+    for maybe_proj in potential_projects:
+        path_dir = os.path.join(dirpath, maybe_proj)
+        proj_fldr = get_project_root_dir(path_dir)
+        if proj_fldr is not None:
+            return_projs.append(proj_fldr)
+        else:
+            children_dirs = list(filter(lambda x: os.path.isdir(os.path.join(path_dir, x)), os.listdir(path_dir)))
+            for child in children_dirs:
+                child_path_dir = os.path.join(path_dir, child)
+                proj_fldr = get_project_root_dir(child_path_dir)
+                if proj_fldr is not None:
+                    return_projs.append(proj_fldr)
+    return return_projs
+
+
+def build_scc_json_for_all_projs(dirpath):
+    projs = load_projects(dirpath)
+    print(f"total projs: {len(projs)}")
+    for pdir in projs:
+        res, o, e = execute_shell_command(f"scc {pdir} -f json > {os.path.join(pdir,'scc.json')}")
+
+
 def main():
     lookup_dir = "/Users/ruirua/repos/pyAnaDroid/demoProjects"
+    build_scc_json_for_all_projs(lookup_dir)
     ls = LanguageStats()
     ls.search_and_parse_files_in_dir(lookup_dir, expected_filename="scc.json")
-    print(json.dumps(ls.language_info, indent=1))
+    #print(json.dumps(ls.language_info, indent=1))
     ls.gen_langs_boxplots_loc()
-    ls.gen_langs_boxplots_cc()
+    #ls.gen_langs_boxplots_cc()
 
 
 if __name__ == '__main__':
