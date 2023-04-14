6e05d9f21097dae4e32a82f6a74e667d6a1e8ba0
Rui Rua <rui.rrua@gmail.com>
2023-04-14 15:14:44 +0100

    bump _version

--- anadroid/_version.py
+++ anadroid/_version.py
@@ -7,5 +7,5 @@ Provides anadroid version information.
 
 from incremental import Version
 
-__version__ = Version("anadroid", 0, 5, 38)
+__version__ = Version("anadroid", 0, 5, 39)
 __all__ = ["__version__"]
